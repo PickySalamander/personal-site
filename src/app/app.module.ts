a733b7aeed625e80000ff77c0dc43d8e66a568c4
Joe Flaherty <joe.flaherty@selleration.com>
2023-03-02 17:53:21 -0500

    yeah... got rid of angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,12 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AppRoutingModule} from './app-routing.module';
 import {RouterModule} from "@angular/router";
-import { HeaderComponent } from './header/header.component';
-import { SidebarComponent } from './sidebar/sidebar.component';
-import { HomeComponent } from './pages/home/home.component';
-import {MatButtonModule} from "@angular/material/button";
-import {MatIconModule} from "@angular/material/icon";
-import {MatRippleModule} from "@angular/material/core";
+import {HeaderComponent} from './header/header.component';
+import {SidebarComponent} from './sidebar/sidebar.component';
+import {HomeComponent} from './pages/home/home.component';
 import {MenuOptionComponent} from "./header/menu-option/menu-option.component";
 
 @NgModule({
@@ -25,10 +21,7 @@ import {MenuOptionComponent} from "./header/menu-option/menu-option.component";
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    RouterModule,
-    MatButtonModule,
-    MatIconModule,
-    MatRippleModule
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
