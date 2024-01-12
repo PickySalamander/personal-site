808610de7ff9e5e8b9fe0688cb95ea0c8675f8a5
Joe Flaherty <joe.flaherty@selleration.com>
2024-01-12 11:25:20 -0500

    updated to new angular control statements

--- src/app/pages/work/project.ts
+++ src/app/pages/work/project.ts
@@ -47,10 +47,10 @@ export const WorkProjects:Project[] = [
     preview: "An Amazon CDK project that sets up a variety of Lambda functions that will scrape a series of websites to see if there are changes.",
     text: [
       "There are a few websites out there that I was interested in watching for updates. Originally, I looked for the usual ways of watching them for changes (social media, RSS, Google Alerts, etc.), but none of them really fit. I also looked into a variety of online tools that already existed and there didn't seem to be any that fulfilled my requirements: pinging the websites on a weekly basis, giving me a list of changes, and most importantly affordable. So I decided to make my own!",
-      "Since one of my requirements was affordability, I decided to make the entire project serverless on the Amazon CDK framework. I've used infrastructure as code frameworks before, and I'm very familiar with CloudFormation. I had never used the CDK before though, so I decided to challenge myself to learn it.",
+      "The application has a backend process that runs in AWS once a week with a complete frontend that reports on the scraping success.",
       "The full source code is <a href='https://github.com/PickySalamander/website-alerter' target='_blank'>here</a>."
     ],
-    code: "AWS CDK TypeScript Docker Node.js",
+    code: "AWS CDK Angular TypeScript Docker Node.js",
     image: "/assets/website-alerter.png"
   },
   {
